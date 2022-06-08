29f73c5f1c06aef6133b54a03ada188353fc490d
teterin <teterin@gk-mic.ru>
2022-06-08 20:48:22 +0300

    Minesweeper task 5 of 16.

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -4,10 +4,24 @@ import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
 
-    private static final int windowSide = 9;
+    private static final int SIDE = 9;
+    private GameObject[][] gameField = new GameObject[SIDE][SIDE];
+    private int countMinesOnField = 0;
 
     public void initialize(){
-        this.setScreenSize(windowSide,windowSide);
+        this.setScreenSize(SIDE,SIDE);
+        createGame();
+    }
+
+    private void createGame(){
+        for(int y=0; y<SIDE; y++){
+            for(int x=0; x<SIDE; x++){
+                boolean isMine = getRandomNumber(10)==5;
+                if(isMine) countMinesOnField++;
+                gameField[y][x] = new GameObject(x,y,isMine);
+                setCellColor(x,y,Color.AQUA);
+            }
+        }
     }
 
 }
