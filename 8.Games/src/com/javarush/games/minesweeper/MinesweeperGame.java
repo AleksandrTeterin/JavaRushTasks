bced546a4dc8387a1d12561011e3ecf2fdbae373
teterin <teterin@gk-mic.ru>
2022-06-08 12:17:34 +0300

    Minesweeper task 2 of 16.

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -4,4 +4,10 @@ import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
 
+    private static final int windowSide = 9;
+
+    public void initialize(){
+        this.setScreenSize(windowSide,windowSide);
+    }
+
 }
